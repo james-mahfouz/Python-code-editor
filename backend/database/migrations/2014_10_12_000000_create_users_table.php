a235e2a2fdc457e58d0725ba652d09a4514d946d
James Mahfouz <jamesmahfouz0@gmail.com>
2023-03-24 11:50:33 +0200

    added the is_admin column to users table

--- backend/database/migrations/2014_10_12_000000_create_users_table.php
+++ backend/database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,10 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('email', 200)->unique();
-            $table->string('name', 60);
-            $table->string('password', 255);
-            $table->integer('is_admin')->nullable();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->integer('is_admin')->default(0);
             $table->timestamps();
         });
     }
