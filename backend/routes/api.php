93bff5df87a0b4d198a8d5010c1baf7c5e232291
mariam-adsaleh <mariamadnansaleh@gmail.com>
2023-03-25 00:03:26 +0200

    added get_developers api

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -16,19 +16,11 @@ Route::group(['prefix' => 'v1'], function(){
 
     Route::group(['middleware' => 'auth:api'], function(){
         Route::post('/logout', [AuthController::class, "logout"]);
-<<<<<<< HEAD
-        Route::get('/get_developers', [UserController::class, "getDevelopers"]);
-        Route::post('/save_code', [UserController::class, "saveCode"]);
-        Route::get('/codes/{user_id}',[UserController::class, "getCode"]);
-        Route::get('/get_all_users', [AdminController::class, "getAllUsers"]);
-
-=======
-        Route::get('/get_developers', [UserController::class, "get_developers"]);
+        Route::post('/get_developers', [UserController::class, "get_developers"]);
         Route::post('/save_code', [UserController::class, "save_code"]);
         Route::post('/chats/{from_user_id}/{to_user_id}', [UserController::class, "send_message"]);
         Route::get('/get_all_users', [AdminController::class, "get_all_users"]);
         Route::get('/codes/{user_id}',[UserController::class, "get_code"]);
->>>>>>> 923f78ddec7276d9d87f045490832f73ef019d02
     });
 
     Route::post('/compile', [CompileController::class, "compile"]);
