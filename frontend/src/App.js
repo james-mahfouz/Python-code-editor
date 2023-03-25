30c0ed1637eb64a216524a9d2b5b22183daecaad
James Mahfouz <jamesmahfouz0@gmail.com>
2023-03-25 14:08:40 +0200

    fixed the routing system in APP

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Route, Router, Routes } from "react-router-dom";
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import './App.css';
 import LandingPage from './Pages/LandingPage';
@@ -15,7 +15,8 @@ function App() {
           <Route path="/signup" element={<SignupPage />} />
           <Route path="*" element={() => <div>404</div>} />
       </Routes>  
-    </Router>  
+    </Router>
+ 
   );
 }
 
