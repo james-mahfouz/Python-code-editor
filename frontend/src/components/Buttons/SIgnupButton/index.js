3f0a477bb9a647d78cc4318c9678acb12057304e
James Mahfouz <jamesmahfouz0@gmail.com>
2023-03-25 19:41:12 +0200

    adjusted the sign up button

--- frontend/src/components/Buttons/SIgnupButton/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "../index.css";
-
-function SignupButton() {
-    return <button className="all-btn signup-btn">SIGNUP</button>;
-}
-
-export default SignupButton;
