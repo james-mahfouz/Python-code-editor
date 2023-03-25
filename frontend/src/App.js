7504e180196f4644a6d79cc9559b9b3c6d294d4d
rima <rima.abouchakra.gms@gmail.com>
2023-03-25 14:15:44 +0200

    edited Input component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,18 +6,18 @@ import LandingPage from './Pages/LandingPage';
 import LoginPage from './Pages/LoginPage';
 import SignupPage from './Pages/SignupPage';
 import SignupButton from './components/Buttons/SignupButton';
-
+import FormInput from './components/FormInput'
 function App() {
   return (
-    <Router>
-      <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/signup" element={<SignupPage />} />
-          <Route path="*" element={() => <div>404</div>} />
-      </Routes>  
-    </Router> 
-   
+    // <Router>
+    //   <Routes>
+    //       <Route path="/" element={<LandingPage />} />
+    //       <Route path="/login" element={<LoginPage />} />
+    //       <Route path="/signup" element={<SignupPage />} />
+    //       <Route path="*" element={() => <div>404</div>} />
+    //   </Routes>  
+    // </Router> 
+   <FormInput/>
   );
 }
 
