cd20fa59f8c9cbfda8f0e6f82c75a6083d3d4bd5
mariam-adsaleh <mariamadnansaleh@gmail.com>
2023-03-26 01:23:10 +0200

    fixed chat component

--- frontend/src/Pages/ChatPage/index.js
+++ frontend/src/Pages/ChatPage/index.js
@@ -1,31 +1,11 @@
-import React, { useState } from 'react';
 
-function Chat() {
-  const [messages, setMessages] = useState([]);
+import Chat from "../../components/Chat";
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const input = event.target.elements.messageInput.value;
-    const newMessage = { text: input, sender: 'user' };
-    setMessages([...messages, newMessage]);
-    event.target.reset();
-  };
-
-  return (
-    <div className="chat">
-      <div className="messages">
-        {messages.map((message, index) => (
-          <div key={index} className={`message ${message.sender}`}>
-            {message.text}
-          </div>
-        ))}
-      </div>
-      <form onSubmit={handleSubmit}>
-        <input type="text" name="messageInput" placeholder="Type your message..." />
-        <button type="submit">Send</button>
-      </form>
-    </div>
-  );
+const ChatPage = () => {
+    return(
+        <div>
+            <Chat />
+        </div>
+    )
 }
-
-export default Chat;
+export default ChatPage
