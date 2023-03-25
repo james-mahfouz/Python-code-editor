f9b2a05e69ef4f789b04f04b149f297580c29739
James Mahfouz <jamesmahfouz0@gmail.com>
2023-03-25 21:56:02 +0200

    added logo and other componant in login page

--- frontend/src/components/FormInput/index.js
+++ frontend/src/components/FormInput/index.js
@@ -1,8 +1,7 @@
-
 import React from 'react';
 
 const FormInput = ({ input }) => {
-    const isPassword = input === "Password";
+    const isPassword = input === "password";
 
     return (
         <div>
--- frontend/src/components/LoginPage/index.js
+++ frontend/src/components/LoginPage/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'; 
 import axios from 'axios';
-
+import NavLogo from '../NavLogo';
+import "./index.css"
 
 
 
@@ -17,28 +18,30 @@ const Login = ()=>{
 
         try {
             const response = await axios.post('http://localhost:8000/api/v1/auth/login', data);
-        
-            alert('Signup successful!');
-
             localStorage.setItem('token', response.data.authorisation.token);
-            } catch (error) {
-                console.error(error);
-                alert('Signup failed. Please try again.');
+        } catch (error) {
+            console.error(error);
         }
     };
 
     return(
-        <form onSubmit={handleSubmit}>
-            <label>Email:</label>
-            <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-            
-            <label>Password:</label>
-            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-            
-            <button type="submit">Submit</button>
-        </form>
+        <div className='form_container'>
+            <NavLogo />
+            <h1>Login</h1>
+            <form className='' onSubmit={handleSubmit}>
+                
+                <div className='input_field'>
+                    <label>Email</label>
+                    <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                </div>
+                <div className='input_field'>
+                    <label>Password</label>
+                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                </div>
+                <button type="submit">Submit</button>
+            </form>
+        </div>
     );
-
 }
 
 export default Login;
