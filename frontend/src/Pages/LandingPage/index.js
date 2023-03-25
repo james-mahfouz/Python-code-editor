1c47d8c5f64d42e8c323943879906f108aba9364
James Mahfouz <jamesmahfouz0@gmail.com>
2023-03-25 14:23:03 +0200

    Adjusted login page

--- frontend/src/Pages/LandingPage/index.js
+++ frontend/src/Pages/LandingPage/index.js
@@ -6,7 +6,7 @@ const LandingPage = () => {
     return(
         <div>
             <h1>Landing Page</h1>
-            <FormInput input='Password'/>
+            <FormInput input='Email'/>
         </div>
     )
 }
