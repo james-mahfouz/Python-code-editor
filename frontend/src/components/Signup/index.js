0a7ed4917767c03cb0aecc6208c343b4e85aa667
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-03-25 17:15:44 +0200

    updated the signup function form

---
+++ frontend/src/components/Signup/index.js
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+import { SignupButton } from '../buttons/SignupButton';
+import axios from 'axios';
+
+
+
+function SignupForm(){
+    const [isFormOpen, setIsFormOpen] = useState(false);
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const formData = {
+        name,
+        email,
+        password,
+        };
+
+        try {
+        const response = await axios.post('http://localhost:8000/api/v1/auth/signup', formData);
+
+        alert('Signup successful!');
+        setIsFormOpen(false);
+
+        // Store the token in local storage
+        localStorage.setItem('token', response.data.authorisation.token);
+        } catch (error) {
+        console.error(error);
+        alert('Signup failed. Please try again.');
+        }
+    };
+
+    return(
+        <div>
+        <SignupButton onClick={() => setIsFormOpen(true)}>Open form</SignupButton>
+
+        {isFormOpen && (
+            <div className="popup">
+            <form onSubmit={handleSubmit}>
+                <label>
+                Name:
+                <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+                </label>
+                <br />
+                <label>
+                Email:
+                <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                </label>
+                <br />
+                <label>
+                Password:
+                <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                </label>
+                <br />
+                <button type="submit">Submit</button>
+                <button onClick={() => setIsFormOpen(false)}>Close form</button>
+            </form>
+            </div>
+        )}
+        </div>
+    );
+
+}
