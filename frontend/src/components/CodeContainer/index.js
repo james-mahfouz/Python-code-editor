9958e5325a90dee4fa3cc35ffafff610dc0c0d8b
James Mahfouz <jamesmahfouz0@gmail.com>
2023-03-26 23:55:32 +0300

    added the save code function

--- frontend/src/components/CodeContainer/index.js
+++ frontend/src/components/CodeContainer/index.js
@@ -5,15 +5,18 @@ import './index.css'
 import axios from 'axios';
 import DownloadButton from '../Buttons/DownloadButton';
 import SaveButton from '../Buttons/SaveButton';
-
+const token = {
+  headers: { Authorization: `Bearer ${localStorage.getItem('token')}` }
+};
 
 const CodeContainer=({selectedCode})=>{
 
-const [code, setCode] = useState('');
-const [output, setOutput] = useState('');
+    const [code, setCode] = useState('');
+    const [output, setOutput] = useState('');
+    const [code_title, setTitle] = useState('');
 
-const handleSubmit = async (e) => {
-    e.preventDefault();
+    const handleSubmit = async (e) => {
+        e.preventDefault();
 
     try {
         const response = await axios.post('http://localhost:8000/api/v1/compile', { code });
@@ -26,24 +29,38 @@ const handleSubmit = async (e) => {
     const handleClear = () => {setOutput('');};
 
     const handleDownload = () => {
-    const blob = new Blob([code], {type: "text/plain;charset=utf-8"});
-    const url = URL.createObjectURL(blob);
-    const a = document.createElement('a');
-    a.download = "my_code.txt";
-    a.href = url;
-    document.body.appendChild(a);
-    a.click();
-    document.body.removeChild(a);
-    URL.revokeObjectURL(url);
-   
-  };
+      const blob = new Blob([code], {type: "text/plain;charset=utf-8"});
+      const url = URL.createObjectURL(blob);
+      const a = document.createElement('a');
+      a.download = "my_code.txt";
+      a.href = url;
+      document.body.appendChild(a);
+      a.click();
+      document.body.removeChild(a);
+      URL.revokeObjectURL(url);
+    };
 
-  useEffect(() => {
-    if (selectedCode !== null) {
-      setCode(selectedCode);
-    }
-  }, [selectedCode]);
-  
+    useEffect(() => {
+      if (selectedCode !== null) {
+        setCode(selectedCode);
+      }
+    }, [selectedCode]);
+
+    const save_code = () => {
+      const data = new FormData()
+      data.append('code', code)
+      data.append('title', code_title)
+
+      try {
+          const response = axios.post('http://localhost:8000/api/v1/save_code', data, token);
+          console.log(response)
+
+      } catch (error) {
+          console.error(error)
+      }
+    };
+    
+    
     return (
       <div>
         <div className="code-container">
@@ -67,8 +84,9 @@ const handleSubmit = async (e) => {
  
         </div>
         <div className='left-editor'>
-           <div onClick={handleDownload}><DownloadButton/></div> 
-            <div><SaveButton/></div>
+            <div onClick={handleDownload}><DownloadButton/></div> 
+            <div onClick={save_code}><SaveButton/></div>
+            <input value={code_title} onChange={(e) => setTitle(e.target.value)} placeholder='enter code title'></input>
         </div>
       </div> 
       );
