1119cd3e078de0e0f36edc176820e6426a9c3da8
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-03-27 02:29:16 +0300

    updated the fetch all users function

--- frontend/src/components/Chat/AllUsers.js
+++ frontend/src/components/Chat/AllUsers.js
@@ -2,51 +2,29 @@ import { useState , useEffect } from "react";
 import axios from 'axios';
 
 
-
-const token = {
-    headers: { Authorization: `Bearer ${localStorage.getItem('token')}` }
-};
 function AllUsers() {
     const [users, setUsers] = useState([]);
     const [selectedUser, setSelectedUser] = useState(null);
+
+
+    useEffect(()=>{
+        const token = localStorage.getItem("token");
+        axios
+    .post("http://localhost:8000/api/v1/get_all_user", {
+        headers: {
+            Authorization: `Bearer ${token}`, 
+        },
+    })
+    .then((response) => {
+        setUsers(response.data.users);
+    })
+    .catch((error) => {
+        console.error(error);
+    });
+    }, []);
+
+
+
+}
+
     
-        useEffect( async () => {
-            
-            try {
-                await axios.post(`http://localhost:8000/api/v1/get_all_user`,token);
-                // setUsers(data.users)
-                alert('Message sent successfully!');
-                
-            } catch (error) {
-                console.log(error);
-                alert('Message not sent.');
-            }
-            
-            // fetch("localhost:8000/api/v1/get_all_user") 
-            // .then((response) => response.json())
-            // .then((data) => setUsers(data.users))
-            // .catch((error) => console.error(error)); 
-        }, []);
-        
-        const handleUserClick = (userId) => {
-        setSelectedUser(userId);
-        };
-    
-        return (
-        <div>
-            <h2>Users</h2>
-            {users.length > 0 ? (
-            users.map((user) => (
-                <div key={user.id} onClick={() => handleUserClick(user.id)}>
-                {user.name}
-                </div>
-            ))
-            ) : (
-            <div>Loading users...</div>
-            )}
-            {selectedUser && <div>Selected User ID: {selectedUser}</div>}
-        </div>
-        );
-    }
-    
-    export default AllUsers;
