4176468b5335815f01eaef6b2a4646a7dc21372f
James Mahfouz <jamesmahfouz0@gmail.com>
2023-03-27 02:16:02 +0300

    added the search by name in the landing page

--- frontend/src/components/NavbarTwo/index.js
+++ frontend/src/components/NavbarTwo/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState } from 'react';
 import NavLogo from '../NavLogo';
 import MessageButton from '../Buttons/MessageButton';
 import LogoutButton from '../Buttons/LogoutButton';
@@ -8,7 +9,7 @@ import { useNavigate } from "react-router-dom";
 import axios from 'axios';
 
 const NavbarTwo=()=> {
-
+  const [name, setName] = useState("")
   const navigate = useNavigate();
 
   const goChat =()=>{
@@ -16,27 +17,38 @@ const NavbarTwo=()=> {
   }
 
   const handleLogout = () => {
-  const token = localStorage.getItem('token');
-  axios.post('http://localhost:8000/api/v1/logout', {}, {
-      headers: {
-        Authorization: `Bearer ${token}`
-      }
-    }).then((response) => {
-      localStorage.removeItem('token');
-      navigate('/');
-    }).catch((error) => {
-      console.log(error);
-    });
+    const token = localStorage.getItem('token');
+    axios.post('http://localhost:8000/api/v1/logout', {}, {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      }).then((response) => {
+        localStorage.removeItem('token');
+        navigate('/');
+      }).catch((error) => {
+        console.log(error);
+      });
   };
-
+  
+  const handleSearch = async () =>{
+    const data = new FormData();
+    data.append('name', name)
+    const response = await axios.post('http://localhost:8000/api/v1/get_developers',data,
+    {
+      headers: {Authorization: `Bearer ${localStorage.getItem('token')}`
+    }})
+    console.log(response.data)
+    
+  }
+  
 return (
     <nav className="navbar">
       <div>
         <NavLogo />
       </div>
       <div className='search-bar'>
-        <input className='search-input' type="text" placeholder='SEARCH...' />
-        <SearchButton/>
+        <input className='search-input' type="text" placeholder='SEARCH...' value={name} onChange={(e) => setName(e.target.value)} />
+        <div onClick={handleSearch}><SearchButton /></div>
       </div>
       <div className="navbar-end">
         <div onClick={goChat}><MessageButton/></div>
