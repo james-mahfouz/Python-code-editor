13f8275355cd2dfb2a13d6136dc8663123faeef2
James Mahfouz <jamesmahfouz0@gmail.com>
2023-03-26 17:20:48 +0300

    refixed chatting page

--- frontend/src/components/Chat/index.js
+++ frontend/src/components/Chat/index.js
@@ -1,58 +1,50 @@
 import React, { useState } from 'react';
-
+import axios from 'axios';
+const token = {
+    headers: { Authorization: `Bearer ${localStorage.getItem('token')}` }
+};
 
 function Chatting() {
     // const [sender, setSender] = useState('');
-    const [reciever, setReciever] = useState('');
-    const [text, setText] = useState('');
+    const [receiver, setReceiver] = useState('');
+    const [text, setMessage] = useState('');
 
     const handleInputChange = (event) => {
         const { name, value } = event.target;
         if (name === "text") {
-          setMessage(value);
+            setMessage(value);
         } else if (name === "receiver") {
-          setReceiver(value);
+            setReceiver(value);
         }
       };
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        // console.log(sender, reciever, text)
         const data = new FormData()
-        data.append('sender', sender)
-        data.append('reciever', reciever)
         data.append('text', text)
+        setReceiver(2)
 
-        //connecting to api's
         try {
-            const response = await axios.post('http://localhost:8000/api/v1/auth/send_message', data);
+            await axios.post(`http://localhost:8000/api/v1/chats/${receiver}`, data,token);
     
             alert('Message sent successfully!');
-    
-            // Store the token in local storage
-            localStorage.setItem('token', response.data.authorisation.token);
-            } catch (error) {
+            
+        } catch (error) {
             console.log(error);
             alert('Message not sent.');
-            }
+        }
     };
 
     return (
         <div>
             <form onSubmit={handleSubmit}>
                 <label>
-                    To:
+                    Message:
                     <input
                         type="text"
-                        name="receiver"
-                        value={receiver}
+                        name="text"
+                        value={text}
                         onChange={handleInputChange} />
-                </label>
-                <br/>
-                <label>
-                    Message:
-                    <textarea name="text" value={text} onChange={handleInputChange}  rows="7" cols="80"></textarea>
-                    
                 </label>
                 <br />
                 <button type="submit">Send Message</button>
