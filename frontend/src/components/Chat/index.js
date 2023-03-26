92675d958629b3a8fd8e23fc0775b25c8a793d5c
mariam-adsaleh <mariamadnansaleh@gmail.com>
2023-03-26 07:18:31 +0300

    Modified chatting functions

--- frontend/src/components/Chat/index.js
+++ frontend/src/components/Chat/index.js
@@ -2,40 +2,67 @@ import React, { useState } from 'react';
 
 
 function Chatting() {
-    const [sender, setSender] = useState('');
-    const [email, setReciever] = useState('');
+    // const [sender, setSender] = useState('');
+    const [reciever, setReciever] = useState('');
     const [text, setText] = useState('');
+
+    const handleInputChange = (event) => {
+        const { name, value } = event.target;
+        if (name === "text") {
+          setMessage(value);
+        } else if (name === "receiver") {
+          setReceiver(value);
+        }
+      };
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        console.log(sender, email, password)
+        // console.log(sender, reciever, text)
         const data = new FormData()
         data.append('sender', sender)
         data.append('reciever', reciever)
         data.append('text', text)
+
+        //connecting to api's
+        try {
+            const response = await axios.post('http://localhost:8000/api/v1/auth/send_message', data);
+    
+            alert('Message sent successfully!');
+    
+            // Store the token in local storage
+            localStorage.setItem('token', response.data.authorisation.token);
+            } catch (error) {
+            console.log(error);
+            alert('Message not sent.');
+            }
     };
 
     return (
         <div>
-        <form onSubmit={handleSubmit}>
-            <label>
-            Name:
-            <input type="text" value={name} onChange={(e) => setSender(e.target.value)} />
-            </label>
-            <br />
-            <label>
-            reciever:
-            <input type="reciever" value={reciever} onChange={(e) => setReciever(e.target.value)} />
-            </label>
-            <br />
-            <label>
-            Text:
-            <input type="text" value={text} onChange={(e) => setText(e.target.value)} />
-            </label>
-            <br />
-            <button type="submit">Send</button>
-        </form>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Message:
+                    <input
+                        type="text"
+                        name="text"
+                        value={text}
+                        onChange={handleInputChange} />
+                </label>
+                <br />
+                <label>
+                    Receiver:
+                    <input
+                        type="text"
+                        name="receiver"
+                        value={receiver}
+                        onChange={handleInputChange} />
+                </label>
+                <br />
+                <button type="submit">Send Message</button>
+            </form>
         </div>
+            
+          
     );
 }
 
