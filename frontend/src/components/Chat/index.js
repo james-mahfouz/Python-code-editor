480fab46a72b1010214bf2f95509ca4fd7f30800
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-03-27 05:50:23 +0300

    removed non important snippets

--- frontend/src/components/Chat/index.js
+++ frontend/src/components/Chat/index.js
@@ -1,40 +1,11 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React from 'react';
 import AllUsers from './AllUsers';
 import SendMessage from './SendMessage';
 
-const token = {
-    headers: { Authorization: `Bearer ${localStorage.getItem('token')}` }
-};
+
 
 function Chatting() {
-    const [receiver, setReceiver] = useState('');
-    const [text, setMessage] = useState('');
 
-    const handleInputChange = (event) => {
-        const { name, value } = event.target;
-        if (name === "text") {
-            setMessage(value);
-        } else if (name === "receiver") {
-            setReceiver(value);
-        }
-        };
-        
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        const data = new FormData()
-        data.append('text', text)
-        setReceiver(2)
-        try {
-            await axios.post(`http://localhost:8000/api/v1/chats/${receiver}`, data,token);
-    
-            alert('Message sent successfully!');
-            
-        } catch (error) {
-            console.log(error);
-            alert('Message not sent.');
-        }
-    };
     return (
         // <div>
         //     <form onSubmit={handleSubmit}>
