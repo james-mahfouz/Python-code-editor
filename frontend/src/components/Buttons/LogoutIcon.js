52e644fe166018f3179e2d196cdf9598addc778b
James Mahfouz <jamesmahfouz0@gmail.com>
2023-03-27 18:45:35 +0300

    finished responsiveness for landing page

--- frontend/src/components/Buttons/LogoutIcon.js
+++ frontend/src/components/Buttons/LogoutIcon.js
@@ -5,7 +5,7 @@ import logoutImage from "../../images/logout.png";
 function MessageIcon() {
     return (
         <button className="all-btn search-btn">
-        <img src={logoutImage} alt="any" />
+        <img src={logoutImage} alt="any" className="logout_icon"/>
         </button>
     );
     }
