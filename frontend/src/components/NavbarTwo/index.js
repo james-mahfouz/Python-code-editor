6809ca115bceb405db40d9c41d3aa2e01a62860c
James Mahfouz <jamesmahfouz0@gmail.com>
2023-03-27 21:38:08 +0300

    added a removal of displayed name when clicking outsid them

--- frontend/src/components/NavbarTwo/index.js
+++ frontend/src/components/NavbarTwo/index.js
@@ -15,7 +15,8 @@ const NavbarTwo=()=> {
   const [name, setName] = useState("")
   const [searched_name, setSearched_name] = useState("")
   const navigate = useNavigate();
-  const searchResultsRef = useRef(null);
+  const searchedNameRef = useRef(null);
+
 
   const goChat =()=>{
     navigate('/chat');
@@ -46,9 +47,19 @@ const NavbarTwo=()=> {
     setSearched_name(response.data.users)
   }
 
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (searchedNameRef.current && !searchedNameRef.current.contains(event.target)) {
+        setSearched_name([]);
+      }
+    };
 
-  
-return (
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+  return (
     <nav className="navbar">
       <div>
         <NavLogo />
@@ -56,7 +67,7 @@ return (
         <div className='search-bar'>
           <input className='search-input' type="text" placeholder='SEARCH...' value={name} onChange={(e) => setName(e.target.value)} />
             <div onClick={handleSearch}><SearchButton /></div>
-            <div className='searched_name'>
+            <div className='searched_name' ref={searchedNameRef}>
             {searched_name && searched_name.map((name, index) => (
                 <div key={name.id}>
                   <div onClick={goChat}> {index}){ name.name }</div><br/>
